545028f4958f00a5d88e24383212d36abf08a1f5
Balwant Padwal <padwal.balwant@gmail.com>
2019-11-19 13:28:49 +0530

    updated port number

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const routes = require( './routes' );
-const port = 8080;
+const port = 8000;
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
